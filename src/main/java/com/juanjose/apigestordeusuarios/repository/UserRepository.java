99d45a2a2ec56f2ee917a19fa971b136ad48a3d5
JuanJose-96 <jjsaVIXXXIII@gmail.com>
2026-01-15 10:57:01 +0100

    Creación de repository

---
+++ src/main/java/com/juanjose/apigestordeusuarios/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.juanjose.apigestordeusuarios.repository;
+
+import com.juanjose.apigestordeusuarios.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
