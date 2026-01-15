99ffab09e9677123f30e44c5aba5e0a9c74027f5
JuanJose-96 <jjsaVIXXXIII@gmail.com>
2026-01-15 10:30:25 +0100

    Base del proyecto

---
+++ src/main/java/com/juanjose/apigestordeusuarios/ApiGestorDeUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.juanjose.apigestordeusuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestorDeUsuariosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGestorDeUsuariosApplication.class, args);
+    }
+
+}
