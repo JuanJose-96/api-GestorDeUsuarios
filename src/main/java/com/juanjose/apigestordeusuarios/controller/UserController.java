31d0f87cefdeebc01db0163d713b526ec7834c16
JuanJose-96 <jjsaVIXXXIII@gmail.com>
2026-01-15 11:05:22 +0100

    Creación del controller

---
+++ src/main/java/com/juanjose/apigestordeusuarios/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.juanjose.apigestordeusuarios.controller;
+
+import com.juanjose.apigestordeusuarios.dto.CreateUserRequestDto;
+import com.juanjose.apigestordeusuarios.dto.ResponseUserDto;
+import com.juanjose.apigestordeusuarios.entity.User;
+import com.juanjose.apigestordeusuarios.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import java.net.URI;
+
+
+@RestController
+@RequestMapping("api/user")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ResponseUserDto> createUser(@Valid @RequestBody CreateUserRequestDto request){
+        ResponseUserDto created = userService.addUser(request);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(created.id())
+                .toUri();
+        return ResponseEntity.created(location).body(created);
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id){
+        User chosen = userService.getUser(id);
+        return ResponseEntity.ok(chosen);
+    }
+
+}
